f16f297a529aff7b59955911c94d7890d911d45e
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:13 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for number-pairs.
+ * ESLint configuration for number-pairs.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
