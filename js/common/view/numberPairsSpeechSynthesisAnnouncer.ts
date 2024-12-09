951002965945cf6f3ac0a08ed5c34d428b9e2b3c
pixelzoom <cmalley@pixelzoom.com>
2024-12-09 13:01:16 -0700

    add numberPairsSpeechSynthesisAnnouncer, https://github.com/phetsims/number-pairs/issues/22

---
+++ js/common/view/numberPairsSpeechSynthesisAnnouncer.ts
@@ -0,0 +1,21 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * An Announcer for speech synthesis in the Number Pairs sim, adapted from numberPlaySpeechSynthesisAnnouncer.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import NumberSuiteCommonSpeechSynthesisAnnouncer from '../../../../number-suite-common/js/common/view/NumberSuiteCommonSpeechSynthesisAnnouncer.js';
+import numberPairs from '../../numberPairs.js';
+import NumberPairsPreferences from '../model/NumberPairsPreferences.js';
+
+const numberPlaySpeechSynthesisAnnouncer = new NumberSuiteCommonSpeechSynthesisAnnouncer(
+  NumberPairsPreferences.isPrimaryLocaleProperty,
+  NumberPairsPreferences.secondLocaleProperty,
+  NumberPairsPreferences.primaryVoiceProperty,
+  NumberPairsPreferences.secondVoiceProperty
+);
+
+numberPairs.register( 'numberPlaySpeechSynthesisAnnouncer', numberPlaySpeechSynthesisAnnouncer );
+export default numberPlaySpeechSynthesisAnnouncer;
