208735263e87e06e5f34b603f975369184efa6ec
Sam Reid <reids@colorado.edu>
2025-04-16 09:52:17 -0600

    Move imports after file doc, see https://github.com/phetsims/perennial/issues/447

--- js/common/model/TGenericNumberPairsModel.ts
+++ js/common/model/TGenericNumberPairsModel.ts
@@ -1,8 +1,5 @@
 // Copyright 2025, University of Colorado Boulder
 
-import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
-import Color from '../../../../scenery/js/util/Color.js';
-
 /**
  * The Number Pairs Model is required to have the below defined signature. This is used to comply with the necessary
  * type requirements needed in view components that are used in Preferences that do not need a full NumberPairsModel
@@ -10,6 +7,9 @@ import Color from '../../../../scenery/js/util/Color.js';
  *
  * @author Marla Schulz (PhET Interactive Simulations)
  */
+
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import Color from '../../../../scenery/js/util/Color.js';
 
 type TGenericNumberPairsModel = {
   totalProperty: TReadOnlyProperty<number>;
