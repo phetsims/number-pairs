5cfecd4a4b938426a4b5150ce6930e753450079e
Sam Reid <reids@colorado.edu>
2025-10-03 10:29:03 -0600

    Remove unused imports from generateFluentTypes, see https://github.com/phetsims/chipper/issues/1636

--- js/NumberPairsFluent.ts
+++ js/NumberPairsFluent.ts
@@ -5,11 +5,10 @@
 /* eslint-disable */
 /* @formatter:off */
 
-import { TReadOnlyProperty } from '../../axon/js/TReadOnlyProperty.js';
 import type { FluentVariable } from '../../chipper/js/browser/FluentPattern.js';
 import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
-import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
 import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
+import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
 import FluentComment from '../../chipper/js/browser/FluentComment.js';
 import numberPairs from './numberPairs.js';
 import NumberPairsStrings from './NumberPairsStrings.js';
