ad5b1c5d0e1803d9c59e9de388b275021e69e81a
Marla Schulz <marla.schulz@colorado.edu>
2025-10-20 16:48:19 -0700

    Refactor KittenNode to simplify includeAttributeSwitch logic, see https://github.com/phetsims/number-pairs/issues/289

--- js/common/view/KittenNode.ts
+++ js/common/view/KittenNode.ts
@@ -13,7 +13,6 @@ import Property from '../../../../axon/js/Property.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import Shape from '../../../../kite/js/Shape.js';
-import affirm from '../../../../perennial-alias/js/browser-and-node/affirm.js';
 import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
@@ -76,23 +75,17 @@ export default class KittenNode extends InteractiveHighlightingNode {
     providedOptions: KittenNodeOptions
   ) {
 
-    const DEFAULT_INCLUDE_ATTRIBUTE_SWITCH = true;
-
-    // Lightweight option for includeAttributeSwitch since it determines accessibleHelpText
-    const includeAttributeSwitch = providedOptions.includeAttributeSwitch === undefined ? DEFAULT_INCLUDE_ATTRIBUTE_SWITCH : providedOptions.includeAttributeSwitch;
-
     const options = optionize<KittenNodeOptions, SelfOptions, NodeOptions>()( {
       tagName: 'div',
       cursor: 'pointer',
       accessibleName: NumberPairsFluent.a11y.kittens.leftAddendKittenStringProperty,
-      accessibleHelpText: includeAttributeSwitch ? NumberPairsFluent.a11y.kittens.accessibleHelpTextStringProperty :
+      accessibleHelpText: providedOptions.includeAttributeSwitch === undefined || providedOptions.includeAttributeSwitch ?
+                          NumberPairsFluent.a11y.kittens.accessibleHelpTextStringProperty :
                           NumberPairsFluent.a11y.kittens.withoutAttributeSwitchHelpTextStringProperty,
       focusable: true,
-      includeAttributeSwitch: DEFAULT_INCLUDE_ATTRIBUTE_SWITCH,
+      includeAttributeSwitch: true,
       dragBounds: KittenNode.DEFAULT_DRAG_BOUNDS
     }, providedOptions );
-
-    affirm( includeAttributeSwitch === options.includeAttributeSwitch, 'includeAttributeSwitch must match' );
 
     // The kittenAttributeSwitch must receive a mutable boolean Property to toggle between two options. Here we create
     // a Property that allows us to toggle between the left and right addend while also still respecting the
