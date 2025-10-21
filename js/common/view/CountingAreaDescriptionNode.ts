b905c8300f8461c29273072a5fa175a17213585a
phet-dev <phetadmin@gmail.com>
2025-10-21 04:22:17 -0600

    update copyright dates from daily grunt work

--- js/common/view/CountingAreaDescriptionNode.ts
+++ js/common/view/CountingAreaDescriptionNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2024-2025, University of Colorado Boulder
+// Copyright 2025, University of Colorado Boulder
 
 /**
  * CountingAreaDescriptionNode provides the accessibility description for the Counting Area.
