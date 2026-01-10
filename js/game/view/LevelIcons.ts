5ca30c41ed6232cded96a217c53f1f5a41df3b4e
phet-dev <phetadmin@gmail.com>
2026-01-10 04:12:24 -0700

    update copyright dates from daily grunt work

--- js/game/view/LevelIcons.ts
+++ js/game/view/LevelIcons.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * LevelIcons are icons representing levels, typically shown in a level selection screen.
