5abae277ef8c9f5c9969df75eea138ef2967b7eb
phet-dev <phetadmin@gmail.com>
2026-01-14 03:58:43 -0700

    update copyright dates from daily grunt work
    (cherry picked from commit 1a8978acf2c5e67d0c4264e350702bb712d58131)

--- js/game/view/AnswerButtonGroup.ts
+++ js/game/view/AnswerButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * AnswerButtonGroup lays out identically sized number buttons in one or two vertical columns.
