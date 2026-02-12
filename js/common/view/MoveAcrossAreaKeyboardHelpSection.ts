77f50cb6886449fe905ed772fc664d66be3db445
phet-dev <phetadmin@gmail.com>
2026-02-12 03:46:16 -0700

    update copyright dates from daily grunt work

--- js/common/view/MoveAcrossAreaKeyboardHelpSection.ts
+++ js/common/view/MoveAcrossAreaKeyboardHelpSection.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 /**
  * Keyboard help section for moving counting objects across an area with home and end keys
  *
--- js/sum/view/CountingObjectSpinnersKeyboardHelpSection.ts
+++ js/sum/view/CountingObjectSpinnersKeyboardHelpSection.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 /**
  * Keyboard help section for the counting object spinners
  *
